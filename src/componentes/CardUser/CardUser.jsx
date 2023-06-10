b6a5fb7e013060f7374e426fb8450e9c0a5b5cf6
jonathan romero fucina <jonathan07_1@hotmail.com>
2023-06-10 10:50:31 -0300

    se trabajo en el proyecto

---
+++ src/componentes/CardUser/CardUser.jsx
@@ -0,0 +1,35 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+const Item = ({ element }) => {
+  return (
+    <Card sx={{ width: 250, height: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={element.img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {element.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {element.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Link to={`/itemDetail/${element.id}`}>
+          <Button size="small" color="primary">
+            Detalles
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+};
