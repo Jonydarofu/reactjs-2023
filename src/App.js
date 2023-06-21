d0bb7b9ed339916e74ebb8ff3635df116c89489f
jonathan romero fucina <jonathan07_1@hotmail.com>
2023-06-20 23:47:08 -0300

    se trabajo en el proyecto react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import Footer from "./componentes/Footer/Footer";
 import CartWidget from "./componentes/CartWidget/CartWidget";
-import Navbar from "./componentes/Navbar/navbar";
+import Navbar from "./componentes/Navbar/Navbar";
 
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BsTypeH1 } from "react-icons/bs";
- 
-function App(){
+
+function App() {
   const onAdd = (cantidad) => {
-    console.log (`se agrrego al carrito ${cantidad} elementos`);
-   }
-   let stock = 10;
-   let initial = 1;
-   return(
+    console.log(`se agrrego al carrito ${cantidad} elementos`);
+  };
+  let stock = 10;
+  let initial = 1;
+  return (
     <div>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path ="/" element={<ItemListContainer />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+        </Routes>
       </BrowserRouter>
     </div>
-   )
+  );
 }
---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ItemList from "../ItemList/ItemList";
+
+const ItemListContainer = () => {
+  const { name } = useParams();
+
+  const [items, setItems] = useState([]);
+
+  const productosFiltrados = products.filter(
+    (elemento) => elemento.category === name
+  );
+  useEffect(() => {
+    const producList = new Promise((resolve, reject) => {
+      resolve(name ? productosFiltrados : products);
+    });
+
+    producList
+
+      .then((res) => {
+        setItems(res);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [name]);
+  console.log(itmes);
+
+  return (
+    <div>
+      <ItemList items={items} />
+    </div>
+  );
+};
